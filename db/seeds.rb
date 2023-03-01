fc218cee0b05d2bf9da9f1732d3e14a4b655e657
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-01 10:21:12 +0300

    generated seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 
 puts "✅ Done seeding!"
+Student.create(name:"jamessdjnc", email:"jamesm@mail.com", phone:1849192734)
+Lecturer.create(name:"jamessdjnc", email:"jamesm@mail.com", phone:1849192734)
